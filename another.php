3bab4f754f853c27c4e25ccbba2dbb466599f039
Keith Benedict <keith@benedicthome.com>
2016-03-11 08:20:53 -0500

    further testing

---
+++ another.php
@@ -0,0 +1,23 @@
+/**
+ * Demo Code to Break stuff
+ */
+class TitanProject {
+
+	private $topics;
+
+	/**
+	 * Class construct
+	 *
+	 * @since 0.0.0
+	 *
+	 */
+	public function __construct() {
+
+		// Add admin hooks 
+		if ( is_admin() ) {
+			add_action( 'admin_menu', array( $this, 'create_settings_menu' ) );
+			add_action( 'admin_init', array( $this, 'register_settings' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
+		}
+	}
+}
