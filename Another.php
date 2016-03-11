3e5d293fb8ed4eb1bca7cb51b778bed98aed7c68
Keith Benedict <keith@benedicthome.com>
2016-03-11 13:23:31 -0500

    another test with escaping

--- Another.php
+++ Another.php
@@ -13,9 +13,13 @@ class TitanProject extends \WPWigit{
 	 *
 	 */
 	public function __construct() {
-
+		$test = 'anyting';
 		// Add admin hooks
 		if ( is_admin() ) {
+			if ( isset( $test ) ) {
+				echo Postmedia\Web\Utilities::esc_layouts( $test ); // should be escaped
+				echo $test; // will fail escaping
+			}
 			add_action( 'admin_menu', array( $this, 'create_settings_menu' ) );
 			add_action( 'admin_init', array( $this, 'register_settings' ) );
 			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
