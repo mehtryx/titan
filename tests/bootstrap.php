d636e9edb826a6f562a53e2e7ee3c0c879535020
Keith Benedict <keith@benedicthome.com>
2016-04-20 13:39:23 -0400

    testing grep

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,9 +6,10 @@ require_once $_tests_dir . '/includes/functions.php';
 
 # Including plugin or theme functionality 
 function _manually_load_plugin_theme() {
-	// require dirname( __FILE__ ) . '/path/to/file_name_goes_here.php';
+	require dirname( __FILE__ ) . '/../pn-widget-device-visibility.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin_theme' );
+
 
 require $_tests_dir . '/includes/bootstrap.php';
 
